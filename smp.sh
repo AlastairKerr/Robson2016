b2344a3a2f41904632e8af086bbf3224ce3e5df1
Alastair Kerr <kerra@bifx4.bio.ed.ac.uk>
2017-02-06 11:52:45 +0000

    Prevent shuffling into unmappable regions

--- smp.sh
+++ smp.sh
@@ -32,15 +32,29 @@ perl sample.pl $1 500 > tmp.bed
 perl sample.pl $2 500 > tmp2.bed 
 
 #using bedtools v2.25.0
-bedtools shuffle  -maxTries 10000 -g hg19.genome -i tmp.bed  -chrom -noOverlapping > tmp-r.bed
-bedtools shuffle  -maxTries 10000 -g hg19.genome -i tmp2.bed  -chrom -noOverlapping > tmp2-r.bed
+bedtools shuffle  -excl hg19_gaps_s.bed \
+	 -maxTries 10000 -g hg19.genome \
+	 -i tmp.bed  -chrom -noOverlapping > tmp-r.bed
 
-#perl -ne 'chomp;@a = split;print "$_\n" if $a[1] < $a[2]' tmp > tmp.bed
-bedtools intersect -wa -u -f $OVERLAP  -b Contactdomainsasbedfile.bed -a tmp.bed|sort -u  | wc -l >> $1.rnd
-bedtools intersect -wa -u -f $OVERLAP  -b Contactdomainsasbedfile.bed -a tmp2.bed|sort -u  | wc -l >> $2.rnd
+bedtools shuffle  -excl hg19_gaps_s.bed \
+	 -maxTries 10000 -g hg19.genome \
+	 -i tmp2.bed  -chrom -noOverlapping > tmp2-r.bed
 
-bedtools intersect -wa -u -f $OVERLAP  -b Contactdomainsasbedfile.bed -a tmp-r.bed|sort -u  | wc -l >> $1.srnd
-bedtools intersect -wa -u -f $OVERLAP  -b Contactdomainsasbedfile.bed -a tmp2-r.bed|sort -u  | wc -l >> $2.srnd
+bedtools intersect -wa -u -f $OVERLAP  \
+	 -b Contactdomainsasbedfile.bed \
+	 -a tmp.bed| sort -u | wc -l >> $1.rnd
+
+bedtools intersect -wa -u -f $OVERLAP \
+	 -b Contactdomainsasbedfile.bed \
+	 -a tmp2.bed| sort -u | wc -l >> $2.rnd
+
+bedtools intersect -wa -u -f $OVERLAP \
+	 -b Contactdomainsasbedfile.bed \
+	 -a tmp-r.bed| sort -u | wc -l >> $1.srnd
+
+bedtools intersect -wa -u -f $OVERLAP \
+	 -b Contactdomainsasbedfile.bed \
+	 -a tmp2-r.bed | sort -u | wc -l >> $2.srnd
 done 
 
 Rscript plot_his.R $1.rnd  $2.rnd $1.srnd  $2.srnd $3
